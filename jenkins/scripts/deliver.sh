82bff41d56d78cb05e9d49a8f77908b7bc383afa
Vitor Hingel <vhahingel@hotmail.com>
2022-09-26 00:10:51 -0300

    showing commands

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,9 +5,9 @@ echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
-set -x
+
 npm run build
-set +x
+
 
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
@@ -17,11 +17,11 @@ echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
 echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
-set -x
+
 npm start &
 sleep 1
 echo $! > .pidfile
-set +x
+
 
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
